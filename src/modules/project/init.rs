63b18de37c55c53cf633a5b4c3e8927fd7559574
The Infinity's <the.infinity.s.infinity@gmail.com>
2025-06-02 09:29:26 +0900

    READMEを読み込む機能を追加。

--- src/modules/project/init.rs
+++ src/modules/project/init.rs
@@ -1,8 +1,14 @@
 use super::metadata::{self, metadata};
 use crate::dprintln;
 pub fn init() -> Result<(), std::io::Error> {
-    let pkg_metadata = metadata().unwrap_or_default();
+    let mut pkg_metadata = metadata().unwrap_or_default();
     let target_dir = metadata::get_dir()?;
-    dprintln!("{}\n{}", pkg_metadata, target_dir.display());
+    let readme_path = target_dir.join("README.md");
+    if readme_path.exists() {
+        let readme_content = std::fs::read_to_string(readme_path)?;
+        pkg_metadata.about.package.description = readme_content;
+        dprintln!("Initialized project metadata.");
+    }
+    
     Ok(())
 }
